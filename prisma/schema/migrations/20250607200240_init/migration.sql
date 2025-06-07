52959aff48ee27f1ed1ca04b7b2675e08b8d4b8d
Qnets <917944345@qq.com>
2025-06-08 05:34:22 +0800

    fix: sql丢失

--- prisma/schema/migrations/20250607200240_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `permission` MODIFY `name` VARCHAR(25) NOT NULL;
-
--- AlterTable
-ALTER TABLE `role` MODIFY `name` VARCHAR(25) NOT NULL;
---
+++ prisma/schema/migrations/20250607213232_init/migration.sql
@@ -0,0 +1,350 @@
+-- CreateTable
+CREATE TABLE `Anime` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(50) NOT NULL,
+    `description` VARCHAR(1000) NOT NULL,
+    `coverUrl` VARCHAR(191) NOT NULL,
+    `bannerUrl` VARCHAR(191) NOT NULL,
+    `status` TINYINT UNSIGNED NOT NULL,
+    `type` TINYINT UNSIGNED NOT NULL,
+    `director` VARCHAR(25) NULL,
+    `cv` VARCHAR(191) NULL,
+    `year` SMALLINT UNSIGNED NOT NULL,
+    `month` TINYINT UNSIGNED NOT NULL,
+    `seasonName` VARCHAR(10) NULL,
+    `season` TINYINT UNSIGNED NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `animeSeriesId` VARCHAR(191) NOT NULL,
+
+    INDEX `Anime_name_idx`(`name`),
+    INDEX `Anime_status_idx`(`status`),
+    INDEX `Anime_type_idx`(`type`),
+    INDEX `Anime_year_idx`(`year`),
+    INDEX `Anime_month_idx`(`month`),
+    INDEX `Anime_createdAt_idx`(`createdAt`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `AnimeSeries` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(50) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `AnimeSeries_name_key`(`name`),
+    INDEX `AnimeSeries_name_idx`(`name`),
+    INDEX `AnimeSeries_createdAt_idx`(`createdAt`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `AnimeBanner` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `animeId` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `AnimeBanner_animeId_key`(`animeId`),
+    INDEX `AnimeBanner_createdAt_idx`(`createdAt`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `AnimeGuide` (
+    `id` VARCHAR(191) NOT NULL,
+    `updateDay` TINYINT UNSIGNED NOT NULL,
+    `updateTime` VARCHAR(8) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `animeId` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `AnimeGuide_animeId_key`(`animeId`),
+    INDEX `AnimeGuide_updateDay_idx`(`updateDay`),
+    INDEX `AnimeGuide_updateTime_idx`(`updateTime`),
+    INDEX `AnimeGuide_createdAt_idx`(`createdAt`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `AnimeRecommend` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(50) NOT NULL,
+    `status` TINYINT UNSIGNED NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    INDEX `AnimeRecommend_name_idx`(`name`),
+    INDEX `AnimeRecommend_status_idx`(`status`),
+    INDEX `AnimeRecommend_createdAt_idx`(`createdAt`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `AnimeRating` (
+    `id` VARCHAR(191) NOT NULL,
+    `score` TINYINT UNSIGNED NOT NULL,
+    `content` VARCHAR(1000) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `animeId` VARCHAR(191) NOT NULL,
+
+    INDEX `AnimeRating_score_idx`(`score`),
+    INDEX `AnimeRating_createdAt_idx`(`createdAt`),
+    UNIQUE INDEX `AnimeRating_userId_animeId_key`(`userId`, `animeId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `AnimeTag` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(25) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `AnimeTag_name_key`(`name`),
+    INDEX `AnimeTag_name_idx`(`name`),
+    INDEX `AnimeTag_createdAt_idx`(`createdAt`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `AnimeCollection` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `animeId` VARCHAR(191) NOT NULL,
+
+    INDEX `AnimeCollection_createdAt_idx`(`createdAt`),
+    UNIQUE INDEX `AnimeCollection_userId_animeId_key`(`userId`, `animeId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Notice` (
+    `id` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(50) NOT NULL,
+    `content` VARCHAR(2500) NOT NULL,
+    `status` TINYINT UNSIGNED NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    INDEX `Notice_title_idx`(`title`),
+    INDEX `Notice_status_idx`(`status`),
+    INDEX `Notice_createdAt_idx`(`createdAt`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `nickname` VARCHAR(25) NOT NULL,
+    `avatar` VARCHAR(191) NULL,
+    `status` TINYINT UNSIGNED NOT NULL DEFAULT 1,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    INDEX `User_status_idx`(`status`),
+    INDEX `User_createdAt_idx`(`createdAt`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Role` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(25) NOT NULL,
+    `role` VARCHAR(50) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Role_role_key`(`role`),
+    INDEX `Role_createdAt_idx`(`createdAt`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Permission` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(25) NOT NULL,
+    `permission` VARCHAR(50) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Permission_permission_key`(`permission`),
+    INDEX `Permission_createdAt_idx`(`createdAt`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Message` (
+    `id` VARCHAR(191) NOT NULL,
+    `type` TINYINT UNSIGNED NOT NULL,
+    `content` VARCHAR(1000) NOT NULL,
+    `reply` VARCHAR(1000) NULL,
+    `status` TINYINT UNSIGNED NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+
+    INDEX `Message_type_idx`(`type`),
+    INDEX `Message_status_idx`(`status`),
+    INDEX `Message_createdAt_idx`(`createdAt`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Video` (
+    `id` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(50) NOT NULL,
+    `episode` INTEGER UNSIGNED NOT NULL,
+    `url` VARCHAR(191) NOT NULL,
+    `playCount` INTEGER UNSIGNED NOT NULL DEFAULT 0,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `animeId` VARCHAR(191) NOT NULL,
+
+    INDEX `Video_episode_idx`(`episode`),
+    INDEX `Video_playCount_idx`(`playCount`),
+    INDEX `Video_createdAt_idx`(`createdAt`),
+    UNIQUE INDEX `Video_animeId_episode_key`(`animeId`, `episode`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `VideoComment` (
+    `id` VARCHAR(191) NOT NULL,
+    `content` VARCHAR(2500) NOT NULL,
+    `likeCount` INTEGER UNSIGNED NOT NULL DEFAULT 0,
+    `replyCount` INTEGER UNSIGNED NOT NULL DEFAULT 0,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `videoId` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `parentId` VARCHAR(191) NULL,
+
+    INDEX `VideoComment_videoId_idx`(`videoId`),
+    INDEX `VideoComment_userId_idx`(`userId`),
+    INDEX `VideoComment_parentId_idx`(`parentId`),
+    INDEX `VideoComment_createdAt_idx`(`createdAt`),
+    INDEX `VideoComment_likeCount_idx`(`likeCount`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `VideoCommentLike` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `userId` VARCHAR(191) NOT NULL,
+    `videoCommentId` VARCHAR(191) NOT NULL,
+
+    INDEX `VideoCommentLike_createdAt_idx`(`createdAt`),
+    UNIQUE INDEX `VideoCommentLike_userId_videoCommentId_key`(`userId`, `videoCommentId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `_AnimeToAnimeRecommend` (
+    `A` VARCHAR(191) NOT NULL,
+    `B` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `_AnimeToAnimeRecommend_AB_unique`(`A`, `B`),
+    INDEX `_AnimeToAnimeRecommend_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `_AnimeToAnimeTag` (
+    `A` VARCHAR(191) NOT NULL,
+    `B` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `_AnimeToAnimeTag_AB_unique`(`A`, `B`),
+    INDEX `_AnimeToAnimeTag_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `_RoleToUser` (
+    `A` VARCHAR(191) NOT NULL,
+    `B` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `_RoleToUser_AB_unique`(`A`, `B`),
+    INDEX `_RoleToUser_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `_PermissionToRole` (
+    `A` VARCHAR(191) NOT NULL,
+    `B` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `_PermissionToRole_AB_unique`(`A`, `B`),
+    INDEX `_PermissionToRole_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Anime` ADD CONSTRAINT `Anime_animeSeriesId_fkey` FOREIGN KEY (`animeSeriesId`) REFERENCES `AnimeSeries`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `AnimeBanner` ADD CONSTRAINT `AnimeBanner_animeId_fkey` FOREIGN KEY (`animeId`) REFERENCES `Anime`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `AnimeGuide` ADD CONSTRAINT `AnimeGuide_animeId_fkey` FOREIGN KEY (`animeId`) REFERENCES `Anime`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `AnimeRating` ADD CONSTRAINT `AnimeRating_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `AnimeRating` ADD CONSTRAINT `AnimeRating_animeId_fkey` FOREIGN KEY (`animeId`) REFERENCES `Anime`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `AnimeCollection` ADD CONSTRAINT `AnimeCollection_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `AnimeCollection` ADD CONSTRAINT `AnimeCollection_animeId_fkey` FOREIGN KEY (`animeId`) REFERENCES `Anime`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Message` ADD CONSTRAINT `Message_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Video` ADD CONSTRAINT `Video_animeId_fkey` FOREIGN KEY (`animeId`) REFERENCES `Anime`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `VideoComment` ADD CONSTRAINT `VideoComment_videoId_fkey` FOREIGN KEY (`videoId`) REFERENCES `Video`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `VideoComment` ADD CONSTRAINT `VideoComment_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `VideoComment` ADD CONSTRAINT `VideoComment_parentId_fkey` FOREIGN KEY (`parentId`) REFERENCES `VideoComment`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `VideoCommentLike` ADD CONSTRAINT `VideoCommentLike_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `VideoCommentLike` ADD CONSTRAINT `VideoCommentLike_videoCommentId_fkey` FOREIGN KEY (`videoCommentId`) REFERENCES `VideoComment`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_AnimeToAnimeRecommend` ADD CONSTRAINT `_AnimeToAnimeRecommend_A_fkey` FOREIGN KEY (`A`) REFERENCES `Anime`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_AnimeToAnimeRecommend` ADD CONSTRAINT `_AnimeToAnimeRecommend_B_fkey` FOREIGN KEY (`B`) REFERENCES `AnimeRecommend`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_AnimeToAnimeTag` ADD CONSTRAINT `_AnimeToAnimeTag_A_fkey` FOREIGN KEY (`A`) REFERENCES `Anime`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_AnimeToAnimeTag` ADD CONSTRAINT `_AnimeToAnimeTag_B_fkey` FOREIGN KEY (`B`) REFERENCES `AnimeTag`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_RoleToUser` ADD CONSTRAINT `_RoleToUser_A_fkey` FOREIGN KEY (`A`) REFERENCES `Role`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_RoleToUser` ADD CONSTRAINT `_RoleToUser_B_fkey` FOREIGN KEY (`B`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_PermissionToRole` ADD CONSTRAINT `_PermissionToRole_A_fkey` FOREIGN KEY (`A`) REFERENCES `Permission`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_PermissionToRole` ADD CONSTRAINT `_PermissionToRole_B_fkey` FOREIGN KEY (`B`) REFERENCES `Role`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
